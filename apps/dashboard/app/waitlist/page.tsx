2c6fec80b312c650895f6ecebe260a478250e99c
southen_ <alabanaomi2020@icloud.com>
2026-04-23 08:31:33 +0100

    fix: final total simplification to standard root Next.js project

--- apps/dashboard/app/waitlist/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const WaitlistContent = dynamic(() => import("./WaitlistContent"), { ssr: false });
-
-export default function WaitlistPage() {
-  return <WaitlistContent />;
-}
--- apps/dashboard/components/ClientNavBar.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from 'next/dynamic';
-
-const NavBar = dynamic(() => import("./NavBar"), { ssr: false });
-
-export default function ClientNavBar() {
-  return <NavBar />;
-}
